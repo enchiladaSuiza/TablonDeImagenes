0b0ad71615c516a2b8fd150062c71cdb40979c3d
18 <18@DESKTOP-OGM3IO3>
2022-03-28 13:32:49 -0600

    Plantilla Clase Usuario

--- src/tablondeimagenes/Usuario.java
+++ src/tablondeimagenes/Usuario.java
@@ -1,5 +1,21 @@
 package tablondeimagenes;
 
+import javax.persistence.*;
+
+@Entity
 public class Usuario {
+    @Id @GeneratedValue
+    private int id;
     
+    public void publicar() {
+        
+    }
+    
+    public void eliminarPublicacion() {
+        
+    }
+    
+    public void responder() {
+        
+    }
 }
